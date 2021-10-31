668357e53f4fbbf4b3bf5523747e29531403375e
surya9901 <surya9901@gmail.com>
2021-10-31 12:50:59 +0530

    heroku added

--- src/settings.js
+++ src/settings.js
@@ -1,11 +1,10 @@
 let getSetting = () => {
     return {
+        api: "https://gmusic-app.herokuapp.com",
+        // api: "http://localhost:3001",
         musicapi: "https://jiosaavn-api-v3.vercel.app/search?query=",
         finalmusicapi: "https://jiosaavn-api-v3.vercel.app/song?id=",
-        github: "https://github.com/surya9901/Music-Player",
-
-        api: "https://gmusic-app.herokuapp.com",
-        // api: "http://localhost:3001"
+        github: "https://github.com/surya9901/Music-Player-Frontend"
     }
 }
 export default getSetting()
